21b0c4fd6277b096ff9ee8b143b4d98568ecf02c
nono38 <74985866+nono38@users.noreply.github.com>
2022-11-28 21:08:21 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+SuperBit.Servo3(SuperBit.enServo.S3, SuperBit.enPos.reverse, 90)
+SuperBit.Servo3(SuperBit.enServo.S2, SuperBit.enPos.forward, 44)
+basic.pause(1000)
+SuperBit.Servo2(SuperBit.enServo.S4, 269)
+basic.forever(function () {
+	
+})
